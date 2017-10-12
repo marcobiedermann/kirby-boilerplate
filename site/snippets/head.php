308471a11e813968dde022f73c1846e6c328b72a
Marco Biedermann <marco.biedermann@gmx.de>
2017-10-12 21:46:13 +0200

    add alternate links

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -6,6 +6,12 @@
   <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
   <meta name="description" content="<?= $site->description()->html() ?>">
 
+  <?php foreach($site->languages() as $language): ?>
+    <?php if ($site->language() !== $language) : ?>
+      <link rel="alternate" hreflang="<?= $language->code(); ?>" href="<?= $page->url($language->code()); ?>" />
+    <?php endif; ?>
+  <?php endforeach ?>
+
   <?= css('assets/css/style.css') ?>
 
 </head>
