632b1d0be85005f8d32755d8057f7ea4a6933b95
Marco Biedermann <marco.biedermann@gmx.de>
2017-10-14 12:49:09 +0200

    add navigation style

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -1,21 +1,21 @@
-<nav>
-  <ul>
+<nav class="navigation">
+  <ul class="navigation__list navigation__list--inline">
     <?php foreach($pages->visible() as $item) : ?>
-    <li>
+    <li class="navigation__item">
       <?php if ($item->isActive()) : ?>
-        <span><?= $item->title(); ?></span>
+        <span class="navigation__link navigation__link--is-active"><?= $item->title(); ?></span>
       <?php else: ?>
-        <a href="<?= $item->url(); ?>"><?= $item->title(); ?></a>
+        <a href="<?= $item->url(); ?>" class="navigation__link"><?= $item->title(); ?></a>
       <?php endif; ?>
     </li>
     <?php endforeach ?>
 
     <?php foreach($site->languages() as $language): ?>
-      <li>
+      <li class="navigation__item">
         <?php if ($site->language() === $language) : ?>
-          <span><?= $language->name(); ?></span>
+          <span class="navigation__link"><?= $language->name(); ?></span>
         <?php else : ?>
-          <a href="<?= $page->url($language->code()) ?>"><?= $language->name(); ?></a>
+          <a href="<?= $page->url($language->code()) ?>" class="navigation__link"><?= $language->name(); ?></a>
         <?php endif; ?>
       </li>
     <?php endforeach ?>
