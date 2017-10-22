46b552c38529e64e2ae170a520be95c3112f8146
Marco Biedermann <marco.biedermann@gmx.de>
2017-10-22 13:42:05 +0200

    add logo

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,6 +7,12 @@
 
     <div class="grid">
 
+      <?php if ($image = $site->logo()->toFile()) : ?>
+        <a href="<?= $site->language()->url(); ?>">
+          <img src="<?= $image->url(); ?>">
+          </a>
+      <?php endif; ?>
+
       <?php snippet('navigation'); ?>
 
     </div>
