9b6355dd6a6f8c7ab10c798b9c4df5a133e4e7d4
Marco Biedermann <marco.biedermann@gmx.de>
2018-01-28 12:48:36 +0100

    add Docker

---
+++ html/site/accounts/admin.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: admin
+password: >
+  $2a$10$7tKXUVN5wMKuM6fajEmTC.d0cdSZZLcrwXhGFQZprP4pbP/JPIVNO
+email: marco.biedermann@gmx.de
+language: en
+role: admin
+history:
+  - blog/feed
+  - blog/article-1
+  - search
+  - home
+  - error
